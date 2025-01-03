6c2cc55c5eb04d4fd3084d1da306e37e4411d5f9
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-01-03 14:26:02 +0700

    Add files via upload

---
+++ 2_text/src/pkg2_text/Main.java
@@ -0,0 +1,15 @@
+
+package pkg2_text;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo World, l'am Adam");
+        System.out.println("Nineteen year");
+        
+        System.out.println("makan");
+        System.out.println("6 + 3");
+        System.out.println(6 + 3);
+    }
+    
+}
