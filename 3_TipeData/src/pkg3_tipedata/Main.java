1f11271b2b122292533005ed70cc87b5b56c81b2
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-01-03 14:42:46 +0700

    Add files via upload

---
+++ 3_TipeData/src/pkg3_tipedata/Main.java
@@ -0,0 +1,31 @@
+
+package pkg3_tipedata;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+       /* 
+        Tipe Data :
+        
+        1.Angka 
+                integer     -> int  -> bilangan bulat
+                floating point -> float bilangan berkoma
+                double          -> double -> bilangan berkoma(tapi bisa lebih banyak)
+       2. text
+                string -> kata/kalimat
+                cahr    -> karakter (A) (%) (+)
+       3. boolean
+                bool -> true/false
+       4. variabel
+        
+       */
+       //tipe data nama_variabel = isi_variabel
+       //tipe data nama_variabel;
+       
+       int umur = 23;
+       System.out.println(umur);
+    }
+    
+}
