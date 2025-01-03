b6a0b3038dcad8b421430db00b4831acb098e6c1
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-01-03 14:43:45 +0700

    Add files via upload

---
+++ 6_escapeSquence/src/pkg6_escapesquence/Main.java
@@ -0,0 +1,24 @@
+
+package pkg6_escapesquence;
+
+public class Main {
+
+    public static void main(String[] args) {
+         String nama = "Adam Zein Aslam";
+         int umur = 19;
+         float berat = (float) 57.5;
+         
+         /*
+         \t -> tab
+         \n -> enter
+         \b -> backspace
+         */
+                 
+         
+         
+        System.out.println("Nama \t= " + nama);
+        System.out.println("Umur \t= " + umur);
+        System.out.println("Berat \t= " + berat);
+    }
+    
+}
