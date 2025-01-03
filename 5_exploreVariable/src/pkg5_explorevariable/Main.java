887f7058839fac7897d76f34b2756b1df93f72ad
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-01-03 14:41:16 +0700

    Add files via upload

---
+++ 5_exploreVariable/src/pkg5_explorevariable/Main.java
@@ -0,0 +1,35 @@
+package pkg5_explorevariable;
+
+
+public class Main {
+
+  
+    public static void main(String[] args) {
+        //integer
+        int umur = 19;
+        System.out.println("Umur = " + umur);
+        
+       //float
+       float berat = (float) 57.5;
+       System.out.println("Berat = " + berat);
+       
+       //double folating
+       double tinggi = 165.333;
+       System.out.println("Tinggi = " + tinggi);
+       
+       //string
+       String nama = "Adam Zein Aslam";
+       System.out.println("Nama = " + nama);
+       
+       //Karakter (char)
+       char jenisKelamin = 'L';
+       System.out.println(jenisKelamin);
+       
+       //boolean
+       boolean ganteng = true;
+       System.out.println("Ganteng = " + ganteng);
+       
+        
+    }
+    
+}
