9cdcdddbf2838d61f1b6cabcb66528eae07b2bab
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-01-04 13:34:57 +0700

    Add files via upload

---
+++ 7_Input/src/pkg7_input/Main.java
@@ -0,0 +1,22 @@
+package pkg7_input;
+import java.util.Scanner;
+        
+        
+public class Main {
+
+   
+    public static void main(String[] args) {
+        int angka;
+        Scanner input = new Scanner (System.in);
+        angka = input.nextInt();
+        
+        System.out.println("angka = " + angka);
+                
+                
+        
+        
+        
+        
+    }
+    
+}
