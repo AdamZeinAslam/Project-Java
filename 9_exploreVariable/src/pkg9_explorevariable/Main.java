e218b6e5c053061d99e11319b64172f5998d06bd
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-01-06 13:45:57 +0700

    Add files via upload

---
+++ 9_exploreVariable/src/pkg9_explorevariable/Main.java
@@ -0,0 +1,22 @@
+
+package pkg9_explorevariable;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+       String nama_depan, nama_belakang, nama_lengkap;
+       nama_depan = "Adam";
+       nama_belakang = "Zein";
+        System.out.print("Nama Depan \t: ");
+        System.out.println(nama_depan);
+        System.out.print("Nama Belakang \t: ");
+        System.out.println(nama_belakang);
+        System.out.print("Nama Lengkap \t: ");
+       nama_lengkap = nama_depan +" "+ nama_belakang;
+       System.out.println(nama_lengkap);
+               
+    }
+    
+}
