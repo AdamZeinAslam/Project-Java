0edccbfa77e516e2956de97546797d7e213a6e3c
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-01-11 19:45:28 +0700

    Add files via upload

---
+++ 15_Asigmment/src/pkg15_asigmment/Main.java
@@ -0,0 +1,30 @@
+
+package pkg15_asigmment;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        
+        =
+        +=
+        -=
+        *=
+        /=
+        %=
+        
+        */
+        
+       int a, b, c;
+       a = 10;
+       b = 5;
+       c = a %= b;
+       
+        System.out.println(c);
+      
+       
+       
+       
+    }
+    
+}
