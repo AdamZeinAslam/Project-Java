ca80e22979e00364195ce986d4be78e67eb9c85a
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-01-13 19:13:51 +0700

    Add files via upload

---
+++ 16_Increment/src/pkg16_increment/Main.java
@@ -0,0 +1,24 @@
+
+package pkg16_increment;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+      /*
+        increment 
+        post incremet = a++
+        pre increment = ++a
+        */ 
+      
+      int a;
+      int i = 1;
+      a = ++i;
+      /*
+      a = i, -> i = i + 1;
+      */
+        System.out.println(a);
+    }
+    
+}
