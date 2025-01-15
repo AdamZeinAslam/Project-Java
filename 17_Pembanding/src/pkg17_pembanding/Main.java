d5732f5d2468595b30723cbc2c8d5b5b87f6bb02
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-01-15 18:14:35 +0700

    Add files via upload

---
+++ 17_Pembanding/src/pkg17_pembanding/Main.java
@@ -0,0 +1,25 @@
+
+package pkg17_pembanding;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 7;
+       
+        // System.out.println(a == b);
+        //System.out.println(a != b);
+        //System.out.println( a > b);
+        //System.out.println( a >= b);
+       
+       /*
+       == : senilai
+       != : tidak senilai
+       > : lebih dari
+       >= : lebih dari sama dengan
+       */
+    }
+    
+}
