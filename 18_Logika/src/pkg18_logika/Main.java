6b38429f63d2a8b067e5cb496edf872ca55c7ab6
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-01-16 19:41:13 +0700

    Add files via upload

---
+++ 18_Logika/src/pkg18_logika/Main.java
@@ -0,0 +1,21 @@
+package pkg18_logika;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+       /*
+        Logika :
+        - NOT (tidak) !
+        - AND (dan) &&
+        - OR (atau) ||
+        */
+       
+       boolean a = true;
+       boolean b = false;
+       
+       boolean c = a || b;
+        System.out.println(c);
+    }
+    
+}
