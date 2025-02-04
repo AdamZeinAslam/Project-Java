60d0060102c18a35dfb9a18f2a92f586d403c801
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-02-04 19:38:03 +0700

    Add files via upload

---
+++ 43_array1/src/pkg43_array1/Main.java
@@ -0,0 +1,14 @@
+
+package pkg43_array1;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       int[] a = new int [4];
+       a [0] = 1;
+       a [1] = 2;
+        System.out.println(a[0]);
+    }
+    
+}
