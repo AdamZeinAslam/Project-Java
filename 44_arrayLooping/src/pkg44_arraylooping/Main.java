8439065ab91f7fbae9b7f420ef71b24e85c58e89
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-02-05 15:33:32 +0700

    Add files via upload

---
+++ 44_arrayLooping/src/pkg44_arraylooping/Main.java
@@ -0,0 +1,16 @@
+package pkg44_arraylooping;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+       int[] a = {1, 2, 3, 4, 5, 6};
+       int jml = 3;
+       for (int i = 0; i < jml; i++){
+        System.out.println(a[i]);   
+       }
+        
+    }
+    
+}
