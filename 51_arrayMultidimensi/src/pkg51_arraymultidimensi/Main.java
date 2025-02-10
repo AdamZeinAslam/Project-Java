546396b3d1d528fb4b08bf1655c8288abbd39372
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-02-10 12:20:50 +0700

    Add files via upload

---
+++ 51_arrayMultidimensi/src/pkg51_arraymultidimensi/Main.java
@@ -0,0 +1,21 @@
+package pkg51_arraymultidimensi;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+      String [] [] kota = {
+        {"Adam",  " Bogor"},
+        {"Zein", " Bandung"}
+        
+        };
+        System.out.print(kota[0][0]);
+        System.out.println(kota[0][1]);
+        System.out.print(kota[1][0]);
+        System.out.println(kota[1][1]);
+    } 
+    
+    }
+    
+
