5d8a714fc94c2898bd85d63fc961e98eaefc2c0a
Adam Zein Aslam <112570925+AdamZeinAslam@users.noreply.github.com>
2025-02-15 20:06:15 +0700

    Add files via upload

---
+++ 61_mathMaksimalminim/src/pkg61_mathmaksimalminim/Main.java
@@ -0,0 +1,16 @@
+
+package pkg61_mathmaksimalminim;
+
+
+public class Main {
+
+  
+    public static void main(String[] args) {
+        double x = 2;
+        double y = 3;
+        double z = Math.max(x, y);
+        System.out.println(z);
+        
+    }
+    
+}
